95d02e0b4be224660acf4c5efe8b23f7c794eb4f
bluepersia <59849802+bluepersia@users.noreply.github.com>
2021-05-03 12:11:22 -0600

    Pointer fix

--- js/app.js
+++ js/app.js
@@ -3,21 +3,23 @@ let selected;
 
 document.getElementById('faq__accordion').addEventListener('click', e => {
 
-    const qa = e.target.closest('.faq__qa');
+    const { target } = e;
 
-    if (qa) {
-        if (selected)
-            selected.classList.remove('faq__qa--selected');
+    if (target.matches('.faq__question')) {
+        const qa = target.closest('.faq__qa');
 
-        if (qa == selected) {
-            selected = null;
-            return;
+        if (qa) {
+            if (selected)
+                selected.classList.remove('faq__qa--selected');
+
+            if (qa == selected) {
+                selected = null;
+                return;
+            }
+
+            qa.classList.add('faq__qa--selected');
+            selected = qa;
         }
-
-        qa.classList.add('faq__qa--selected');
-        selected = qa;
     }
-
-
 
 });
